91932fdc99dda739ef902c740ba5883d56a21dde
Dineth Nimnaka <dinethnimnaka1@gmail.com>
2026-09-21 21:50:46 +0530

    fix(sasl): report skipped listeners accurately

--- internal/sasl/server.go
+++ internal/sasl/server.go
@@ -117,6 +117,8 @@ func (s *Server) Start() error {
 		if err := s.startUnixListener(); err != nil {
 			return fmt.Errorf("failed to start UNIX listener: %w", err)
 		}
+		log.Printf("Started Unix socket listener: %s", s.socketPath)
+	} else {
 		log.Printf("Skipping Unix socket listener (scope: %s, only TCP connections are allowed)", s.saslScope)
 	}
 
@@ -125,6 +127,8 @@ func (s *Server) Start() error {
 		if err := s.startTCPListener(); err != nil {
 			return fmt.Errorf("failed to start TCP listener: %w", err)
 		}
+		log.Printf("Started TCP listener: %s", s.tcpAddr)
+	} else {
 		log.Printf("Skipping TCP listener (scope: %s, only Unix socket connections are allowed)", s.saslScope)
 	}
 
